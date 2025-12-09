0207de720899c8e754f1e14f56824a63f6589c92
Chase J <contact@chasej.xyz>
2025-12-08 18:01:34 -0800

    Moksha checker now has timetamps , nice formatting, writes results to file

--- moksha_checker.py
+++ moksha_checker.py
@@ -1,7 +1,9 @@
 import re
 import urllib.request
+import datetime
 
-# opens file where you put each moksha url in a new line and turns it into an array
+# opens file where you put each moksha url in a new line and turns it
+# into an array
 f = open("moksha_URLS")
 raw_urls = f.read()
 f.close()
@@ -11,17 +13,32 @@ url_array =raw_urls.split()
 pattern1 = r"(?<=QueuePosition<\/th><td>)\d+"
 pattern2 = r"(?<=SubmissionStatus-)\w+"
 
+# opens/creates output file for results w/timestamp (appends, so you can
+# see how little you moved from the last time you checked!)
+o = open(file="moksha_output",mode="a+")
+o.write(f"{datetime.datetime.now()}\n{"-"*25}\n")
+
+# inits for the core loop
 i = 0
+market = ""
+queue_num = ""
 
 for i in range(len(url_array)):
-    # opens URL and saves to html_str
+    # opens URL and saves as string
     fp = urllib.request.urlopen(url_array[i])
     my_bytes = fp.read()
     html_str = my_bytes.decode("utf8")
     fp.close()
-    # strips out whitespace to make one big ol str
+    # strips out whitespace
     html_str =html_str.strip()
     html_str =html_str.replace(" ","")
     html_str =html_str.replace("\t","")
     html_str =html_str.replace("\n","")
-    print(f"{re.findall(pattern2,html_str)}\t\t {re.findall(pattern1,html_str)}")
+    # run regex and output results to terminal + file
+    market = str(re.findall(pattern2,html_str))
+    queue_num = str(re.findall(pattern1,html_str))
+    print(f"{market[2:-2]:<20}{queue_num[2:-2]:>10}")
+    o.write(f"{market[2:-2]:<20}{queue_num[2:-2]:>10}\n")
+
+# closes output file
+o.close()

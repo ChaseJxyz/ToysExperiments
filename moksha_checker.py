8e16fafc633d4a59d7074a05f8f8d20050f7b25c
Chase J <contact@chasej.xyz>
2025-12-12 00:04:08 -0800

    Added status checking to moksha checker + prettified the text a little.

--- moksha_checker.py
+++ moksha_checker.py
@@ -2,43 +2,88 @@ import re
 import urllib.request
 import datetime
 
-# opens file where you put each moksha url in a new line and turns it
-# into an array
+# opens file with 1 moksha url per line + converts into
+# array
 f = open("moksha_URLS")
 raw_urls = f.read()
 f.close()
-url_array =raw_urls.split()
+url_array = raw_urls.split()
 
-# regex for finding market name + queue position
-pattern1 = r"(?<=QueuePosition<\/th><td>)\d+"
-pattern2 = r"(?<=SubmissionStatus-)\w+"
+# regex for finding market name + queue position + sub status
+queue_pattern = r"(?<=QueuePosition<\/th><td>)\d+"
+market_pattern = r"(?<=SubmissionStatus-)\w+"
+status_pattern = r"(?<=<th>Status<\/th><td>)\w+"
+
+# silly dict for prettifying results
+# feel free to remove the emoji
+results = {
+    # i only have rejected/accepted urls to test so idk if
+    # the others work yet
+    "rejected": "Rejected 😿",
+    "accepted": "🎉Accepted🎉",
+    "revisionrequested": "Revision Requested 🙀",
+    "withdrawn": "Withdrawn 🐈‍⬛",
+    "inprogress": "In Progress 😺",
+}
+
+"""
+Additional moksha statuses (that are probably backend-only, but storing
+here in case they might be of use later):
+Open, Closed, Archived, Claimed, File Error, Needs Second Opinion,
+Not recommended, Recommended, Unclaimed
+"""
+
+
+# turns array into string
+def delister(list):
+    return "".join(list)
+
+
+# puts whitespaces back into market name
+# it a'int perfect but it's close
+def despacer(name):
+    return re.sub(r"(?<=\w)([A-Z])", r" \1", name)
+
 
 # opens/creates output file for results w/timestamp (appends, so you can
 # see how little you moved from the last time you checked!)
-o = open(file="moksha_output",mode="a+")
-o.write(f"{datetime.datetime.now()}\n{"-"*25}\n")
+o = open(file="moksha_output", mode="a+")
+o.write(f"{datetime.datetime.now()}\n{'-' * 25}\n")
 
 # inits for the core loop
 i = 0
 market = ""
-queue_num = ""
+sub_status = ""
 
 for i in range(len(url_array)):
-    # opens URL and saves as string
+    # opens/closes URL and saves as string
     fp = urllib.request.urlopen(url_array[i])
     my_bytes = fp.read()
     html_str = my_bytes.decode("utf8")
     fp.close()
-    # strips out whitespace
-    html_str =html_str.strip()
-    html_str =html_str.replace(" ","")
-    html_str =html_str.replace("\t","")
-    html_str =html_str.replace("\n","")
-    # run regex and output results to terminal + file
-    market = str(re.findall(pattern2,html_str))
-    queue_num = str(re.findall(pattern1,html_str))
-    print(f"{market[2:-2]:<20}{queue_num[2:-2]:>10}")
-    o.write(f"{market[2:-2]:<20}{queue_num[2:-2]:>10}\n")
+    # strips out whitespace from webpage
+    html_str = html_str.strip()
+    html_str = html_str.replace(" ", "")
+    html_str = html_str.replace("\t", "")
+    html_str = html_str.replace("\n", "")
+    # find/save/format market name
+    market = re.findall(market_pattern, html_str)
+    market = delister(market)
+    market = despacer(market)
+    # looks for queue position
+    sub_status = re.findall(queue_pattern, html_str)
+    # runs if no digits (i.e. closed sub or market doesn't share positions)
+    # find/save/format sub status
+    if sub_status == []:
+        sub_status = object = re.findall(status_pattern, html_str)
+        sub_status = delister(sub_status)
+        sub_status = results[sub_status]
+    else:
+        sub_status = delister(sub_status)
+    # print results to terminal
+    print(f"{market:<20}{sub_status:>10}")
+    # writes results to output file
+    o.write(f"{market[2:-2]:<20}{sub_status[2:-2]:>10}\n")
 
 # closes output file
 o.close()
--- readme.md
+++ readme.md
@@ -4,17 +4,16 @@ These are fun little (non-IF) based things I'm making just to see if I can (and 
 
 ### tictactoe.py
 
-Tic-tac-toe in the terminal. The goal is to update it after each week of my Intro to Scripting class to reflect the things I've learned (or, the things that I feel confident enough in teaching/figuring out myself).
-
+Tic-tac-toe in the terminal. The goal is to update it after each week of my Intro to Scripting class to reflect the things I've learned (or, the things that I feel confident enough in teaching/figuring out myself). EDIT: probably finished with this, because it's already over-engineered to all hell.
 
 ### amiraizer.py
 
-Command line script to take a text file and convert it into the reality-resistant shorthand of one of my characters (so I don't have to do it myself!). In/out is printed in the terminal, but output is also appened to the file. The only non-alphanumeric char that's converted is "-" while others (including whitespace) are left as is (intentional).
+Python script to take a text file and convert it into the reality-resistant shorthand of one of my characters (so I don't have to do it myself!). In/out is printed in the terminal, but output is also appended to the file. The only non-alphanumeric char that's converted is "-" while others (including whitespace) are left as is (intentional).
 
 ### higher_lower_game.py
 
-I was supposed to make pseudocode for this for my IT 140 homework...but I wanted to make sure it was correct....so I just programmed the whole thing. oops
+I was only supposed to make pseudocode for this for my IT 140 homework...but I wanted to make sure it was correct....so I just programmed the whole thing. oops
 
 ### moksha_checker.py
 
-Moksha is a submission tracking system for writers. Each submission is given a URL with unique sub id/user id that can be used to check the status of a given submission (namely, what number in "line" you are). To save writers time from having to open/refresh a bunch of URLs, I made a basic script to batch-check a number of URLs and return the queue position (since it's less sus for a single user to make multiple requests sometimes than a central website making hundreds/thousands of requests per day, and also these links should be considered secrets and not handed to 3rd parties since they can be used by anyone to withdraw a submission). Prints the market's name + queue number to terminal.
+Moksha is a writing submission tracking/management system for literary journals. Each submission is given a URL with unique sub/user id that allows the writer to check the status of a specific submission (namely, what number in "line" you are and if they've rejected you yet). To save writers time from having to open/refresh a bunch of URLs (and taking advantage that all of these URLs are public), I wrote a script to batch-check a series of URLs and return the queue position (since it's less sus for a single user to make multiple requests sometimes than a central website making hundreds/thousands of requests per day. And also these links should be considered secrets/not handed to 3rd parties, since they can be used by anyone to withdraw a submission)(they should fix that). Prints the market's name + and queue position OR submission status. Probably want to make it read your file/highlight changes in position since you've last run it.

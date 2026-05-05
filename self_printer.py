4fd301d7c3a2f85f9e384ce0c864aad17c433e09
Chase J <85197722+ChaseJxyz@users.noreply.github.com>
2026-05-04 18:56:55 -0700

    test

--- self_printer.py
+++ self_printer.py
@@ -1,2 +1,17 @@
+#!/usr/bin/env python3
 # this code prints itself!
+import subprocess
 
+# c =
+
+r = subprocess.run(
+    [
+        "curl",
+        "https://raw.githubusercontent.com/ChaseJxyz/ToysExperiments/refs/heads/main/self_printer.py",
+    ],
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE,
+    text=True,
+)
+
+print(r.stdout)
